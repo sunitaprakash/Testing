ed6fbf344f769f6a3ff321a6bdecf30dc6569f9b
Sunita <sunitav@gmail.com>
2021-11-18 12:11:02 -0800

    update file

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import os
-import hydra
   
 print(os.getenv('GRID_URL:' "GRID_URL"))
 print('testing env')
+print('List of file in current direcoty"')
+path ='.'
+dir_list= os.listdir(path)
+print(dir_list)
